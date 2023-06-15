7e84f351f2bf3c6374167528a0df84a21a25473e
Yahan Zhang <78954178+katherineyahan@users.noreply.github.com>
2023-06-15 11:11:39 +0800

    Add files via upload

---
+++ A3_prs.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 11:06:44 2023
+
+@author: 张雅涵
+"""
+import pandas as pd
+import numpy as np
+import math
+
+LD = pd.read_csv("C:/Users/yahan/Desktop/COMP565/A3/data/LD.csv")
+beta = pd.read_csv("C:/Users/yahan/Desktop/COMP565/A3/data/beta_marginal.csv")
+beta.rename(columns={'Unnamed: 0':'SNP'}, inplace=True)
+SNPs=beta["SNP"].tolist()
+beta = beta.set_index("SNP", drop = False)
+del beta[beta.columns[0]]
+LD.rename(columns={'Unnamed: 0':'SNP'}, inplace=True)
+LD = LD.set_index("SNP", drop = False)
+del LD[LD.columns[0]]
+LD.values[[np.arange(LD.shape[0])]*2] = 1
+#Q1 Expectation Step
+#SNPs=beta["SNP"].tolist()
+colnames=["SNP"]
+VI_SNPs=pd.DataFrame(list(SNPs),columns=colnames)
+VI_SNPs['posterior precision']=1
+VI_SNPs['posterior mean']=0
+VI_SNPs['PIP']=0.01
+VI_SNPs = VI_SNPs.set_index("SNP", drop = False)
+del VI_SNPs[VI_SNPs.columns[0]]
+t_epsilon=1
+t_beta=200
+pi=0.01
+N=439
+M=100
+ELBO_list=[]
+for cycle in range(10):
+ for i in SNPs:
+    #update the posterior precison
+    pos_pre=(N*t_epsilon)+t_beta
+    VI_SNPs.loc[i,"posterior precision"]=pos_pre
+
+    #update the posterior mean of the effect size
+    b_i=beta.loc[i,"V1"]
+    copy_SNPs=SNPs.copy()
+    copy_SNPs.remove(i)
+    total=0.0
+    df_without_i=VI_SNPs.copy()
+    df_without_i.drop(i,axis=0,inplace=True)
+    pos_mean_term1=(df_without_i['PIP']*df_without_i['posterior mean']*LD[i]).sum()
+   # for j in copy_SNPs:       
+    #    r=LD.loc[i,j]
+     #   mul= VI_SNPs.loc[j,'PIP']*VI_SNPs.loc[j,'posterior mean']*r
+      #  print(mul)
+       # total=total+mul
+    
+    pos_mean_beta=N*(t_epsilon/VI_SNPs.loc[i,"posterior precision"])*(b_i-pos_mean_term1)
+    VI_SNPs.loc[i,"posterior mean"]=pos_mean_beta
+    #update the PIP
+    #first calculate the mean j
+    mean_j=(math.log(pi/(1-pi)))+((1/2)*math.log(t_beta/VI_SNPs.loc[i,"posterior precision"]))+((VI_SNPs.loc[i,"posterior precision"]/2)*((VI_SNPs.loc[i,"posterior mean"])**2))
+    pip=1/(1+math.exp(-(mean_j)))
+    VI_SNPs.loc[i,"PIP"]=pip
+
+    
+    #if γj < 0.01 set it to 0.01, and if γj > 0.99 set it to 0.99.
+ for i in SNPs:
+    if(VI_SNPs.loc[i,"PIP"]>0.99):
+        VI_SNPs.loc[i,"PIP"]=0.99
+    elif(VI_SNPs.loc[i,"PIP"]<0.01):
+        VI_SNPs.loc[i,"PIP"]=0.01
+        
+ #Q2 Maximization Step
+ pi=(VI_SNPs["PIP"].sum())/M
+ t_beta_term1=VI_SNPs['PIP']*(VI_SNPs['posterior mean'].to_numpy()**2+(1/(VI_SNPs['posterior precision'].to_numpy())))
+ gamma=VI_SNPs['PIP'].sum()
+ t_beta=1/(t_beta_term1.sum()/gamma)
+        
+ #Q3 Evidence lower bound
+
+ #calculate the first term in ELBO
+ term1=(N/2)*(math.log(t_epsilon))
+
+ term2=(t_epsilon/2)*N
+ 
+ term3_1=(VI_SNPs['PIP']*VI_SNPs['posterior mean']).to_numpy()
+ term3=t_epsilon * np.matmul(np.transpose(term3_1), N*beta['V1'])
+
+ term4_sum=(VI_SNPs['PIP']*(VI_SNPs['posterior mean']**2+(1/VI_SNPs['posterior precision']))).to_numpy()
+
+ term4=N*(t_epsilon/2)*term4_sum.sum()
+
+ list2=[]
+ for a in SNPs:#from j=1 to M
+   if(SNPs.index(a)<(len(SNPs)-1)):
+    #k=SNPs.index(a)+1
+    list1=[]
+    #print(j)
+    for k in range(SNPs.index(a)+1,len(SNPs)):#from k=j+1 to M
+        product=VI_SNPs.loc[a,'PIP']*VI_SNPs.loc[a,'posterior mean']*VI_SNPs.loc[SNPs[k],'PIP']*VI_SNPs.loc[SNPs[k],'posterior mean']*(N*LD.loc[a,SNPs[k]])
+        list1.append(product)
+    list1_sum=sum(list1)
+    list2.append(list1_sum)
+ list2_sum=sum(list2)
+ term5=list2_sum*t_epsilon
+
+ ELBO_1=term1-term2+term3-term4-term5
+
+ #calculate the second term in ELBO
+ ELBO_2=(t_beta/2)*(term4_sum.sum())
+
+ #calculate the third term in ELBO
+ ELBO_3=-(1/2)*math.log(t_beta)*((VI_SNPs['PIP'].to_numpy()).sum())
+
+ #calculate the fourth term in ELBO
+ ELBO4_term1=math.log(pi)*VI_SNPs['PIP']+(1-VI_SNPs['PIP'])*math.log(1-pi)
+ ELBO_4=ELBO4_term1.sum()
+
+ #calculate the fifth term in ELBO
+ ELBO5_term1=VI_SNPs['PIP']*np.log(VI_SNPs['PIP'])+(1-(VI_SNPs['PIP']))*np.log(1-VI_SNPs['PIP'])
+ ELBO_5=ELBO5_term1.sum()
+
+ #calculate the ELBO
+ ELBO=ELBO_1+ELBO_2-ELBO_3+ELBO_4-ELBO_5
+ ELBO_list.append(ELBO)
+
+#Plot the scatter plot
+import matplotlib.pyplot as plt
+x=[0,1,2,3,4,5,6,7,8,9]
+plt.scatter(x,ELBO_list)
+#Q4 Evaluate PRS prediction
+x_train=pd.read_csv("C:/Users/yahan/Desktop/COMP565/A3/data/X_train.csv")
+y_train=pd.read_csv("C:/Users/yahan/Desktop/COMP565/A3/data/y_train.csv")
+x_test=pd.read_csv("C:/Users/yahan/Desktop/COMP565/A3/data/X_test.csv")
+y_test=pd.read_csv("C:/Users/yahan/Desktop/COMP565/A3/data/y_test.csv")
+
+x_train =x_train.to_numpy()[:, 1:]
+element_wise=VI_SNPs['PIP']*VI_SNPs['posterior mean']
+element_wise=element_wise.to_numpy()
+y_train_estimate=np.matmul(x_train,element_wise)
+
+x_test=x_test.to_numpy()[:, 1:]
+y_test_estimate=np.matmul(x_test,(VI_SNPs['PIP']*VI_SNPs['posterior mean']).to_numpy())
+
+#Calculate pearson correlation coefficient
+y_train_estimate=list(y_train_estimate)
+from scipy.stats import pearsonr
+corr, _ = pearsonr(y_train_estimate, y_train['V1'])
+x=y_train_estimate
+y=y_train['V1']
+coefficients = np.polyfit(x, y, 1)
+p = np.poly1d(coefficients)
+plt.scatter(x,y)
+plt.plot(x, p(x), 'r')
+plt.show()
+
+x=list(y_test_estimate)
+y=y_test['V1']
+coefficients = np.polyfit(x, y, 1)
+p = np.poly1d(coefficients)
+plt.scatter(x,y)
+plt.plot(x, p(x), 'r')
+plt.show()
+corr, _ = pearsonr(y_test_estimate, y_test['V1'])
+
+
+#Q5 Evaluate fine-mapping
+my_list = [i for i in range(0, 100)]
+plt.scatter(my_list,list(VI_SNPs['PIP']))
+plt.scatter(SNPs.index('rs9482449'),VI_SNPs.loc['rs9482449','PIP'],color='red')
+plt.scatter(SNPs.index('rs7771989'),VI_SNPs.loc['rs7771989','PIP'],color='red')
+plt.scatter(SNPs.index('rs2169092'),VI_SNPs.loc['rs2169092','PIP'],color='red')
+plt.show()
---
+++ A5_LDA.py
@@ -0,0 +1,206 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  5 14:07:05 2023
+
+@author: 张雅涵
+"""
+
+#NAME: Yahan Zhang
+#Student ID: 260908840
+import pandas as pd
+import numpy as np
+from seaborn import heatmap
+import matplotlib.pyplot as plt
+import seaborn as sns
+df_mimics3 = pd.read_csv("./MIMIC3_DIAGNOSES_ICD_subset.csv")
+df_mimics3['topic 1']=0
+df_mimics3['topic 2']=0
+df_mimics3['topic 3']=0
+df_mimics3['topic 4']=0
+df_mimics3['topic 5']=0
+
+#construct a patients x topics dataframe
+patients=df_mimics3['SUBJECT_ID'].unique()
+df_DK=pd.DataFrame(patients, columns=['patients'])
+df_DK = df_DK.set_index('patients', drop = True)
+df_DK['topic 1']=0
+df_DK['topic 2']=0
+df_DK['topic 3']=0
+df_DK['topic 4']=0
+df_DK['topic 5']=0
+#construct a ICD-codes x topics dataframe
+ICD_codes=df_mimics3['ICD9_CODE'].unique()
+df_WK=pd.DataFrame(ICD_codes,columns=['ICD'])
+df_WK=df_WK.set_index('ICD',drop=True)
+df_WK['topic 1']=0
+df_WK['topic 2']=0
+df_WK['topic 3']=0
+df_WK['topic 4']=0
+df_WK['topic 5']=0
+
+M=389
+D=689
+alpha=1
+beta=0.001
+K=5
+#Zid is one-hot encoded
+for j in range(10):
+ print(j)
+ 
+ for i, row in df_mimics3.iterrows():
+    #find the current patient
+    patient=df_mimics3.iloc[i,0]
+    #find the current ICD_code
+    ICD=df_mimics3.iloc[i,1]
+    #update the topic distribution    
+    gamma_idk=np.zeros(5)
+  
+    for topic in range(1,6):
+       topic_name='topic '+str(topic)
+       col_number=(topic-1)+2
+       
+        
+       n_dk=df_DK.loc[patient,topic_name]-df_mimics3.iloc[i,col_number]
+       n_wk_sum=df_WK[topic_name].sum()-df_mimics3.iloc[i,col_number]
+       n=df_WK.loc[ICD,topic_name]-df_mimics3.iloc[i,col_number]
+       
+        #calculate gamma idk
+       gamma_term1=alpha+n_dk
+      
+       gamma_term2=(beta+n)/(M*beta+n_wk_sum)
+       gamma_k=gamma_term1*gamma_term2
+       gamma_idk[topic-1]=gamma_k
+       
+    gamma_idk = gamma_idk / gamma_idk.sum()
+
+    z_id=np.random.multinomial(1,gamma_idk,size=1)
+    z_id=z_id.tolist()
+    #update the z_id to the original file
+    #find the index when z_id=1
+    index_of_one = z_id[0].index(1)
+    topic='topic '+str(index_of_one+1)
+    df_mimics3.loc[i,topic]=1
+    #update the sufficient statistics
+    df_DK.loc[patient,topic]+=1
+    df_WK.loc[ICD,topic]+=1
+ 
+ 
+    
+ 
+
+#normalize the final ICDs-by-topics and the patients-by-topics matrix
+phi_WK=pd.DataFrame(ICD_codes,columns=['ICD'])
+phi_WK=phi_WK.set_index('ICD',drop=True)
+phi_WK['topic 1']=0
+phi_WK['topic 2']=0
+phi_WK['topic 3']=0
+phi_WK['topic 4']=0
+phi_WK['topic 5']=0
+
+for i in range(phi_WK.shape[0]):
+    ICD = phi_WK.index[i]
+    for k in range(1,6):
+        topic='topic '+str(k)
+        n_wk=df_WK.loc[ICD,topic]
+        sum_n_wk=df_WK[topic].sum()
+        phi_WK.loc[ICD,topic]=(beta+n_wk)/((M*beta)+sum_n_wk)
+        
+theta_DK=pd.DataFrame(patients,columns=['patients'])
+theta_DK=theta_DK.set_index('patients',drop=True)
+theta_DK['topic 1']=0
+theta_DK['topic 2']=0
+theta_DK['topic 3']=0
+theta_DK['topic 4']=0
+theta_DK['topic 5']=0
+
+for i in range(theta_DK.shape[0]):
+    patient = theta_DK.index[i]
+    for k in range(1,6):
+        topic='topic '+str(k)
+        n_dk=df_DK.loc[patient,topic]
+        sum_n_dk=df_DK.iloc[i].sum()
+        theta_DK.loc[patient,topic]=(alpha+n_dk)/((K*alpha)+sum_n_dk)
+        
+
+#visualize the top ICD codes under each topic
+topics_ICD_matrix=pd.DataFrame()
+for i in range(1,6):
+    topic='topic '+str(i)
+    top5 = phi_WK.nlargest(10,topic)
+    topics_ICD_matrix = topics_ICD_matrix.append(top5)
+#find the corresponding disease
+df_ICD_titles = pd.read_csv("./D_ICD_DIAGNOSES_DATA_TABLE.csv")
+
+for i in range(topics_ICD_matrix.shape[0]):
+    print(i)
+    current_ICD=topics_ICD_matrix.index[i]
+    try:
+      idx = df_ICD_titles.index[df_ICD_titles['ICD9_CODE'] == str(current_ICD)].tolist()[0]
+      title = df_ICD_titles.loc[idx, 'SHORT_TITLE']
+      concatenate = str(current_ICD) + "-" + str(title)
+      topics_ICD_matrix.rename(index={current_ICD: concatenate}, inplace=True)
+    except IndexError:
+      print("ICD code not found in df_ICD_titles:", current_ICD)
+fig, ax = plt.subplots(figsize=(10, 20))
+ICD_topics = heatmap(topics_ICD_matrix, cmap="Reds", linecolor="white")
+
+#correlate topics with the target ICD codes
+
+#construct a matrix as patientsx3
+patient_3icd=pd.DataFrame(patients,columns=['patients'])
+patient_3icd=patient_3icd.set_index('patients',drop=True)
+patient_3icd['331']=0
+patient_3icd['332']=0
+patient_3icd['340']=0
+
+
+for patient in patients:
+    #find the related ICD code of this patient
+    ICD_codes=df_mimics3.loc[df_mimics3['SUBJECT_ID'] == patient , 'ICD9_CODE']
+    ICD_codes=ICD_codes.tolist()
+    #check if they contain code starts with 331,332 or 340
+    for code in ICD_codes:
+        first_three_ints = str(code)[:3]
+        if(first_three_ints=='331'):
+            patient_3icd.loc[patient,'331']=1
+        if(first_three_ints=='332'):
+            patient_3icd.loc[patient,'332']=1
+        if(first_three_ints=='340'):
+             patient_3icd.loc[patient,'340']=1
+             
+# merge the two dataframes on PatientID
+merged_df = pd.merge(theta_DK, patient_3icd, on='patients')
+
+#creat topics by ICD-codes matrix
+topics_ICD=pd.DataFrame()
+topics_ICD['331']=0
+topics_ICD['332']=0
+topics_ICD['340']=0
+ICD=['331','332','340']
+topics=['topic 1','topic 2','topic 3','topic 4','topic 5']
+for icd in ICD:
+    for topic in topics:
+        topics_ICD.loc[topic,icd]=merged_df[icd].corr(merged_df[topic])
+
+
+fig_Q3, ax_Q3 = plt.subplots(figsize=(5, 3))
+ax_Q3 = heatmap(topics_ICD, cmap="Reds", linecolor="white")
+    
+
+
+#Generate the heatmap for the top 100 patients
+topics_patients_matrix=pd.DataFrame()
+for i in range(1,6):
+    topic='topic '+str(i)
+    top100 = theta_DK.nlargest(100,topic)
+    top100 = top100.fillna(0)
+    topics_patients_matrix = topics_patients_matrix.append(top100)
+topics_patients_matrix.fillna(0)            
+fig_Q4, ax_Q4 = plt.subplots(1,figsize=(10, 30))
+ht_Q4= heatmap(topics_patients_matrix, cmap="Reds", linecolor="white")
+     
+     
+        
+    
+        
+        
